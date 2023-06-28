1ab954a8758cc4537a8222bafaa5478114ce84ae
macalafuente <macarenalafuente3@gmail.com>
2023-06-27 21:16:57 -0300

    empezamos script descomprimir.sh

--- descomprimir.sh
+++ descomprimir.sh
@@ -3,4 +3,14 @@
 #poder indicar por argumento dos archivos (uno con las imágenes y otro
 #con una suma de verificación). Si ocurrió algún error se debe informar al
 #usuario de lo contrario se procede a descomprimir.
-~                                                    
+
+imag_comprimidas=$1 
+suma_verificacion=$2
+
+#Tiene que ver con la salida de error que echo $? ???
+
+if $imag_comprimidas -eq *.gz|*.bz2;
+     #descomprime tar xvf <mi_dir.tar> 
+else: 
+       echo "mensaje error"  	
+	
